0383190e0d07b10b93c4964cb4932563ca5ea720
Hong <chong@clevelandohio.gov>
2022-03-14 08:56:56 -0400

    Initial add files 3/14/2022

---
+++ ClevHousingStudy.R
@@ -0,0 +1,23 @@
+# 1 population 2015 to 2019/2020 - percent change past five years in Cities in the Cuyahoga County
+# population density 
+
+
+
+# 2 Race distribution
+
+
+# 3 Low moderate income family - house hold income, family income, per capita by city
+
+# 4 Low mod income cencentation in latest layer HUD layer??
+
+
+# 5 Housing stock 
+#  age
+
+
+# 6 historic district and building
+
+
+
+
+
