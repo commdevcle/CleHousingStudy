533dbfa8956584ab6ff892f275911b33775a832a
Hong <chong@clevelandohio.gov>
2022-03-15 16:53:06 -0400

    update commit

--- ClevHousingStudy_Pop.R
+++ ClevHousingStudy_Pop.R
@@ -5,7 +5,7 @@ library(ggplot2)
 v20 <- load_variables(2019, "acs5", cache = TRUE)
 view(v20)
 
-
+#------------------------------------------------------------------------------------------------------
 # 1 population 2015 to 2019/2020 - percent change past five years in Cities in the Cuyahoga County
 # population density 
 # B01001_001 Estimate!!Total:
@@ -112,7 +112,8 @@ names(mytable)[5] <- '%Change10-19'
 names(mytable)[6] <- '%Change15-19'
 
 
-#------- Visualizing over time population
+#---------------------------------------------------------------------------------------------- 
+#Visualizing over time population total population
 
 total_population_10 <- get_decennial(
   geography = "county subdivision", 
@@ -128,8 +129,6 @@ total_population_10 <- get_decennial(
 total_population_10
 
 
-
-
 years <- 2010:2019
 names(years) <- years
 years
@@ -140,7 +139,7 @@ cnty_value <- map_dfr(years, ~{
     state = "OH",
     county = "Cuyahoga",
     year = .x,
-    survey = "acs1"
+    survey = "acs5"
   )
 }, .id = "year")
 
@@ -153,8 +152,8 @@ g1 <- ggplot(cnty_value, aes(x = year, y = estimate, group = 1)) +
   scale_y_continuous(labels = scales::number) + 
   labs(title = "Populaton change in Cuyahoga Couty, Ohio from 2010 to 2019",
        x = "Year",
-       y = "ACS 1 Year Estimate",
-       caption = "Source: ACH 1 Year Estimae from 2010 to 2019")
+       y = "ACS 5 year estimate",
+       caption = "Source: ACH 5 Year Estimae from 2010 to 2019")
 
 
 cle_value <- map_dfr(years, ~{
@@ -164,7 +163,7 @@ cle_value <- map_dfr(years, ~{
     state = "OH",
     county = "Cuyahoga",
     year = .x,
-    survey = "acs1"
+    survey = "acs5"
   )%>% 
     filter (GEOID == "3903516000")%>% 
     mutate(NAME = str_remove(NAME, ", Cuyahoga County, Ohio")) 
@@ -180,8 +179,8 @@ g2 <- ggplot(cle_value, aes(x = year, y = estimate, group = 1)) +
   scale_y_continuous(labels = scales::number) + 
   labs(title = "Populaton change in Cleveland, Cuyahoga Couty, Ohio from 2010 to 2019",
        x = "Year",
-       y = "ACS 1 Year Estimate",
-       caption = "Source: ACH 1 Year Estimae from 2010 to 2019")
+       y = "ACS 5 year estimate",
+       caption = "Source: ACH 5 Year Estimae from 2010 to 2019")
 
 
 #--- combine two data in one plot
@@ -198,7 +197,7 @@ g3 <- ggplot(NULL, aes(x= year, y = estimate, group = 1)) +
   labs(title = "Populaton change in Cleveland and Cuyahoga County, Ohio from 2010 to 2019",
        x = "Year",
        y = "ACS estimate",
-       caption = "Source: ACH 1 Year Estimae from 2010 to 2019")
+       caption = "Source: ACH 5 Year Estimae from 2010 to 2019")
 
 g3         
 
@@ -211,7 +210,7 @@ g3 | (g1 / g2)
 
 
 
-
+#----------------------------------------------------------------------------------------------------------
 ## 2 median age
 #------- median age 	B01002_001
 years <- 2010:2019
@@ -416,7 +415,11 @@ g3
 g3 | (g2 / g1)
 
 #----------------------------
-# 4-2 median household income B19049_001 Estimate!!Median household income in the past 12 months (in 2019 inflation-adjusted dollars) --!!Total:
+# 4-2 median household income 
+# B19049_001 Estimate!!Median household income in the past 12 months (in 2019 inflation-adjusted dollars) --!!Total:
+# B22008_001 Estimate!!Median household income in the past 12 months (in 2019 inflation-adjusted dollars)--!!Total:
+# B25119_002 Estimate!!Median household income in the past 12 months (in 2019 inflation-adjusted dollars) --!!Total:!!Owner occupied (dollars)
+# B25119_003 Estimate!!Median household income in the past 12 months (in 2019 inflation-adjusted dollars) --!!Total:!!Renter occupied (dollars)
 
 years <- 2010:2019
 names(years) <- years
@@ -497,4 +500,91 @@ g3
 #install.packages("patchwork")
 #library(patchwork)
 g3 | (g2 / g1)
+
+
+
+#---- 4.3 Median nonfamily household income
+# B19215_001 Estimate!!Median nonfamily household income in the past 12 months (in 2019 inflation-adjusted dollars) --!!Total (dollars):
+
+years <- 2010:2019
+names(years) <- years
+years
+
+cnty_value <- map_dfr(years, ~{
+  get_acs(
+    geography = "county",
+    variables = "B19215_001",
+    state = "OH",
+    county = "Cuyahoga",
+    year = .x,
+    survey = "acs5"
+  )
+}, .id = "year")
+
+cnty_value
+
+g1 <- ggplot(cnty_value, aes(x = year, y = estimate, group = 1)) + 
+  geom_line() + 
+  geom_point()+
+  geom_text(data = cnty_value, aes(x= year,label= estimate), color="black")+
+  theme_minimal(base_size = 10) + 
+  scale_y_continuous(labels = scales::dollar) + 
+  labs(title = "Median Nonfamily Household income in Cuyahoga Couty, Ohio from 2010 to 2019",
+       x = "Year",
+       y = "ACS 5 year estimate",
+       caption = "Source: ACH 1 Year Estimae from 2010 to 2019")
+g1
+
+cle_value <- map_dfr(years, ~{
+  get_acs(
+    geography = "county subdivision",
+    variables = "B19215_001",
+    state = "OH",
+    county = "Cuyahoga",
+    year = .x,
+    survey = "acs5"
+  )%>% 
+    filter (GEOID == "3903516000")%>% 
+    mutate(NAME = str_remove(NAME, ", Cuyahoga County, Ohio")) 
+}, .id = "year")
+
+cle_value
+
+g2 <- ggplot(cle_value, aes(x = year, y = estimate, group = 1)) + 
+  geom_line() + 
+  geom_point()+
+  geom_text(data = cle_value, aes(x= year,label= estimate), color="black")+
+  theme_minimal(base_size = 10) + 
+  scale_y_continuous(labels = scales::dollar) + 
+  labs(title = "Median Nonfamily Household Income in Cleveland, Cuyahoga Couty, Ohio from 2010 to 2019",
+       x = "Year",
+       y = "ACS 5 year estimate",
+       caption = "Source: ACH 5 Year Estimae from 2010 to 2019")
+
+g2
+
+#--- combine two data in one plot
+
+g3 <- ggplot(NULL, aes(x= year, y = estimate, group = 1)) +    
+  geom_line(data = cnty_value, col = "red") + 
+  geom_point(data = cnty_value, col = "black")+
+  #geom_text(data = cnty_value, aes(x= year,label= estimate), color="black")+
+  geom_line(data = cle_value, col = "blue")+ 
+  geom_point(data = cle_value, col = "black") +
+  #geom_text(data = cle_value, aes(x= year,label= estimate), color="black")+
+  theme_minimal(base_size = 10) + 
+  scale_y_continuous(labels = scales::dollar) + 
+  labs(title = "Median Nonfamily Household Income in Cleveland and Cuyahoga County, Ohio from 2010 to 2019",
+       x = "Year",
+       y = "ACS 5 year estimate",
+       caption = "Source: ACH 5 Year Estimae from 2010 to 2019")
+
+g3         
+
+
+#install.packages("patchwork")
+#library(patchwork)
+g3 | (g2 / g1)
+
+
 
